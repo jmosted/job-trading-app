18efb0763fb9231e296599060588d004b3fda857
jmosted <jose.roman@unas.edu.pe>
2024-07-16 10:22:27 -0500

    Auto-generated commit

---
+++ assets/env.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  window.__env.ENGINE_AUTH = 'http://localhost:55000/api/v1/auth';
+  window.__env.ENGINE_OFFER = 'http://localhost:55000/api/v1/transaction';
+
+  window.__env.enableDebug = true;
+})(this);
